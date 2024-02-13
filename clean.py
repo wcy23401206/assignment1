c4d405f86f0879e785f9d99ad279f425f972e157
WANG CHUANGYI <23401206@life.hkbu.edu.hk>
2024-02-13 19:35:22 +0800

    data cleaning script

---
+++ clean.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import argparse
+
+def cleaning(input1,input2,output):
+    df1 = pd.read_csv(input1)
+    df2 = pd.read_csv(input2)
+    merge_data = pd.merge(df1,df2,left_on="respondent_id", right_on="id")
+    merge_data = merge_data.dropna()
+    merge_data = merge_data[~merge_data['job'].str.contains('insurance|Insurance')]
+    merge_data.drop('id', axis=1, inplace=True)
+    merge_data.to_csv(output, index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input1", help="respondent_contact")
+    parser.add_argument("input2", help="respondent_other")
+    parser.add_argument("output", help="output file")
+    args = parser.parse_args()
+    cleaning(args.input1, args.input2, args.output)
